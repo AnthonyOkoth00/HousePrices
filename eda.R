a8bcbf9fca78080567b477921fac30ab1b290b04
Anthony Okoth <misc4tony089@gmail.com>
2018-03-05 23:20:37 +0100

    Second highest correlation between Grade (Ground) Living Area (square feet) and the Sale Price

--- eda.R
+++ eda.R
@@ -36,5 +36,9 @@ cor_sorted <- as.matrix(sort(cor_numVar[, "SalePrice"], decreasing = TRUE))
 CorHigh <- names(which(apply(cor_sorted, 1, function(x) abs(x)>0.5)))
 cor_numVar <- cor_numVar[CorHigh, CorHigh]
 corrplot.mixed(cor_numVar, tl.col = "black", tl.pos = "lt")
-
+# Correlation between the feture "Overall Quality", which has the highest correlation, and the SalePrice
 ggplot(data = all[!is.na(all$SalePrice), ], aes(x = factor(OverallQual), y = SalePrice)) + geom_boxplot() + labs(x = "Overall Quality") + scale_y_continuous(breaks = seq(0, 800000, by = 100000), labels = comma)
+
+# Second highest correlation between Grade (Ground) Living Area (square feet) and the Sale Price
+ggplot(data = all[!is.na(all$SalePrice), ], aes(x = GrLivArea, y = SalePrice)) + geom_point(col = "blue") + geom_smooth(method = "lm", se = FALSE, color = "black", aes(group = 1)) +
+  + scale_y_continuous(breaks = seq(0, 800000, by = 100000), labels = comma)
