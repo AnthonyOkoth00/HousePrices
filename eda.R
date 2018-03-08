42a9424123eb21a32707e3359f16a0919388f48f
Anthony Okoth <misc4tony089@gmail.com>
2018-03-08 11:51:15 +0100

    Random Forest and visualizations.

--- eda.R
+++ eda.R
@@ -355,3 +355,106 @@ cor_sorted <- as.matrix(sort(cor_numVar[, "SalePrice"], decreasing = TRUE))
 CorHigh <- names(which(apply(cor_sorted, 1, function(x) abs(x) > 0.5)))
 cor_numVar <- cor_numVar[CorHigh, CorHigh]
 corrplot.mixed(cor_numVar, tl.col = "black", tl.pos = "lt", tl.cex = 0.7, cl.cex = .7, number.cex = .7)
+
+##Finding variable importance with a quick Random Forest
+set.seed(2018)
+quick_RF <- randomForest(x = all[1:1460, -79], y = all$SalePrice[1:1460], ntree = 100, importance = TRUE)
+imp_RF <- importance(quick_RF)
+imp_DF <- data.frame(Variables = row.names(imp_RF), MSE = imp_RF[,1])
+imp_DF <- imp_DF[order(imp_DF$MSE, decreasing = TRUE), ]
+ggplot(imp_DF[1:20, ], aes(x = reorder(Variables, MSE), y = MSE, fill = MSE)) +
+  geom_bar(stat = "identity") + 
+  labs(x = "Variables", y = "% increase MSE if variable is randomly permuted") +
+  coord_flip() +
+  theme(legend.position = "none")
+s1 <- ggplot(data= all, aes(x=GrLivArea)) +
+  geom_density() + labs(x='Square feet living area')
+s2 <- ggplot(data=all, aes(x=as.factor(TotRmsAbvGrd))) +
+  geom_histogram(stat='count') + labs(x='Rooms above Ground')
+s3 <- ggplot(data= all, aes(x=X1stFlrSF)) +
+  geom_density() + labs(x='Square feet first floor')
+s4 <- ggplot(data= all, aes(x=X2ndFlrSF)) +
+  geom_density() + labs(x='Square feet second floor')
+s5 <- ggplot(data= all, aes(x=TotalBsmtSF)) +
+  geom_density() + labs(x='Square feet basement')
+s6 <- ggplot(data= all[all$LotArea<100000,], aes(x=LotArea)) +
+  geom_density() + labs(x='Square feet lot')
+s7 <- ggplot(data= all, aes(x=LotFrontage)) +
+  geom_density() + labs(x='Linear feet lot frontage')
+layout <- matrix(c(1,2,5,3,4,8,6,7),4,2,byrow=TRUE)
+multiplot(s1, s2, s3, s4, s5, s6, s7,layout=layout)
+n1 <- ggplot(all[!is.na(all$SalePrice),], aes(x=Neighborhood, y=SalePrice)) +
+  geom_bar(stat='summary', fun.y = "median", fill='blue') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_y_continuous(breaks= seq(0, 800000, by=50000), labels = comma) +
+  geom_label(stat = "count", aes(label = ..count.., y = ..count..), size=3) +
+  geom_hline(yintercept=163000, linetype="dashed", color = "red") #dashed line is median SalePrice
+n2 <- ggplot(data=all, aes(x=Neighborhood)) +
+  geom_histogram(stat='count')+
+  geom_label(stat = "count", aes(label = ..count.., y = ..count..))+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+grid.arrange(n1, n2)
+q1 <- ggplot(data=all, aes(x=as.factor(OverallQual))) +
+  geom_histogram(stat='count')
+q2 <- ggplot(data=all, aes(x=as.factor(ExterQual))) +
+  geom_histogram(stat='count')
+q3 <- ggplot(data=all, aes(x=as.factor(BsmtQual))) +
+  geom_histogram(stat='count')
+q4 <- ggplot(data=all, aes(x=as.factor(KitchenQual))) +
+  geom_histogram(stat='count')
+q5 <- ggplot(data=all, aes(x=as.factor(GarageQual))) +
+  geom_histogram(stat='count')
+q6 <- ggplot(data=all, aes(x=as.factor(FireplaceQu))) +
+  geom_histogram(stat='count')
+q7 <- ggplot(data=all, aes(x=as.factor(PoolQC))) +
+  geom_histogram(stat='count')
+layout <- matrix(c(1,2,8,3,4,8,5,6,7),3,3,byrow=TRUE)
+multiplot(q1, q2, q3, q4, q5, q6, q7, layout=layout)
+ms1 <- ggplot(all[!is.na(all$SalePrice),], aes(x=MSSubClass, y=SalePrice)) +
+  geom_bar(stat='summary', fun.y = "median", fill='blue') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_y_continuous(breaks= seq(0, 800000, by=50000), labels = comma) +
+  geom_label(stat = "count", aes(label = ..count.., y = ..count..)) +
+  geom_hline(yintercept=163000, linetype="dashed", color = "red") #dashed line is median SalePrice
+ms2 <- ggplot(data=all, aes(x=MSSubClass)) +
+  geom_histogram(stat='count')+
+  geom_label(stat = "count", aes(label = ..count.., y = ..count..)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+grid.arrange(ms1, ms2)
+all$MSSubClass[all$MSSubClass=='1,5 story PUD all'] <- '2 story PUD 1946+'
+#correct error
+all$GarageYrBlt[2593] <- 2007 #this must have been a typo. GarageYrBlt=2207, YearBuilt=2006, YearRemodAdd=2007.
+g1 <- ggplot(data=all[all$GarageCars !=0,], aes(x=GarageYrBlt)) +
+  geom_histogram()
+g2 <- ggplot(data=all, aes(x=as.factor(GarageCars))) +
+  geom_histogram(stat='count')
+g3 <- ggplot(data= all, aes(x=GarageArea)) +
+  geom_density()
+g4 <- ggplot(data=all, aes(x=as.factor(GarageCond))) +
+  geom_histogram(stat='count')
+g5 <- ggplot(data=all, aes(x=GarageType)) +
+  geom_histogram(stat='count')
+g6 <- ggplot(data=all, aes(x=as.factor(GarageQual))) +
+  geom_histogram(stat='count')
+g7 <- ggplot(data=all, aes(x=as.factor(GarageFinish))) +
+  geom_histogram(stat='count')
+layout <- matrix(c(1,5,5,2,3,8,6,4,7),3,3,byrow=TRUE)
+multiplot(g1, g2, g3, g4, g5, g6, g7, layout=layout)
+b1 <- ggplot(data=all, aes(x=BsmtFinSF1)) +
+  geom_histogram() + labs(x='Type 1 finished square feet')
+b2 <- ggplot(data=all, aes(x=BsmtFinSF2)) +
+  geom_histogram()+ labs(x='Type 2 finished square feet')
+b3 <- ggplot(data=all, aes(x=BsmtUnfSF)) +
+  geom_histogram()+ labs(x='Unfinished square feet')
+b4 <- ggplot(data=all, aes(x=as.factor(BsmtFinType1))) +
+  geom_histogram(stat='count')+ labs(x='Rating of Type 1 finished area')
+b5 <- ggplot(data=all, aes(x=as.factor(BsmtFinType2))) +
+  geom_histogram(stat='count')+ labs(x='Rating of Type 2 finished area')
+b6 <- ggplot(data=all, aes(x=as.factor(BsmtQual))) +
+  geom_histogram(stat='count')+ labs(x='Quality Rating')
+b7 <- ggplot(data=all, aes(x=as.factor(BsmtCond))) +
+  geom_histogram(stat='count')+ labs(x='Rating of general condition')
+b8 <- ggplot(data=all, aes(x=as.factor(BsmtExposure))) +
+  geom_histogram(stat='count')+ labs(x='Walkout or garden level walls')
+layout <- matrix(c(1,2,3,4,5,9,6,7,8),3,3,byrow=TRUE)
+multiplot(b1, b2, b3, b4, b5, b6, b7, b8, layout=layout)
