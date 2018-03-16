2ce0609012c0f0621028370dfaa4703d14ede1ba
Anthony Okoth <misc4tony089@gmail.com>
2018-03-16 13:02:25 +0100

    visualizations of correlations between features and scatter plots of the 10 highest correlating features

--- python-eda.py
+++ python-eda.py
@@ -14,7 +14,7 @@ df_train = pd.read_csv("./train.csv")
 #descriptive statistics summary
 print df_train["SalePrice"].describe()
 
-sns.distplot(df_train["SalePrice"])
+#sns.distplot(df_train["SalePrice"])
 #plt.show()
 
 
@@ -28,11 +28,44 @@ print "------------------------------------------------"
 #scatter plot grlivarea/saleprice
 var = "GrLivArea"
 data = pd.concat([df_train["SalePrice"], df_train[var]], axis= 1)
-data.plot.scatter(x = var, y = "SalePrice", ylim = (0.800000))
+#data.plot.scatter(x = var, y = "SalePrice", ylim = (0.800000))
 #plt.show()
 
 #scatter plot totalbsmtsf/saleprice
 var = "TotalBsmtSF"
 data = pd.concat([df_train["SalePrice"], df_train[var]], axis= 1)
-data.plot.scatter(x = var, y = "SalePrice", ylim = (0.800000))
+#data.plot.scatter(x = var, y = "SalePrice", ylim = (0.800000))
+
+#box plot overallqual/saleprice
+var = "OverallQual"
+data = pd.concat([df_train["SalePrice"], df_train[var]], axis = 1)
+#f, ax = plt.subplots(figsize = (8, 6))
+#fig = sns.boxplot(x = var, y = "SalePrice", data = data)
+#fig.axis(ymin= 0, ymax= 800000)
+
+
+var = "YearBuilt"
+data = pd.concat([df_train["SalePrice"], df_train[var]], axis = 1)
+f, ax = plt.subplots(figsize = (16, 8))
+#fig = sns.boxplot(x = var, y = "SalePrice", data = data)
+#fig.axis(ymin= 0, ymax= 800000)
+plt.xticks(rotation = 90)
+
+#correlation matrix
+corrmat = df_train.corr()
+#f, ax = plt.subplots(figsize = (12, 9))
+#sns.heatmap(corrmat, vmax= .8, square= True)
+
+#saleprice correlation matrix
+k = 10 #number of variables for heatmap
+cols = corrmat.nlargest(k, "SalePrice")["SalePrice"].index
+cm = np.corrcoef(df_train[cols].values.T)
+#sns.set(font_scale= 1.25)
+#hm = sns.heatmap(cm, cbar= True, annot= True, square= True, fmt= ".2f", annot_kws= {"size": 10},
+                 yticklabels= cols.values, xticklabels= cols.values)
+
+#scatterplot
+sns.set()
+cols = ["SalePrice", "OverallQual", "GrLivArea", "GarageCars", "TotalBsmtSF", "FullBath", "YearBuilt"]
+sns.pairplot(df_train[cols], size= 2.5)
 plt.show()
